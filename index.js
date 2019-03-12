be5e8fbcd31fd42b36d61c56ee5f4b4fd7aaa047
Andrew Carpenter <andrew@audioinhd.com>
2019-03-12 00:26:23 -0400

    Update deps, use mvt for tests

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
-// Setup
+'use strict'
+
 var leven = require('leven')
 
-// Helpers
 var levMinInAry = function (array, src) {
   var min = 1000
   var len = array.length
+
   for (var counter = 0; counter < len; counter++) {
     var val = array[counter]
+
     if (val && val.length && val.length > 0) {
       var levScore = leven(src, array[counter])
       if (levScore < min) min = levScore
     }
   }
+
   return min
 }
 
-// Export main function
 module.exports = function (ary, src1, key1, src2, key2) {
   return ary.sort(function (a, b) {
     if (key1 instanceof Array) {
       var aLev = levMinInAry(key1.map(function (k) { return a[k] }), src1)
       var bLev = levMinInAry(key1.map(function (k) { return b[k] }), src1)
+
       return aLev - bLev
     }
+
     if (!key1 && !key2) return leven(src1, a) < leven(src1, b) ? -1 : 1
+
     if (!key2) return leven(src1, a[key1]) < leven(src1, b[key1]) ? -1 : 1
+
     var score = 0
     var a1 = leven(src1, a[key1])
     var b1 = leven(src1, b[key1])
     var a2 = leven(src2, a[key2])
     var b2 = leven(src2, b[key2])
+
     if (a1 < b1) score = score - 10
     if (a1 > b1) score = score + 10
     if (a2 < b2) score = score - 1
     if (a2 > b2) score = score + 1
+
     return score
   })
 }
--- test.js
+++ test.js
@@ -1,95 +1,109 @@
 'use strict'
 
-import test from 'ava'
-import levenSort from './index'
+const { runTests, test } = require('mvt')
+const levenSort = require('./index')
 
-test(`sorts simple text array`, (assert) => {
-  let sourceName = 'Bill Griffin'
-  let nameAry = [
-    'Carl Martinez',
-    'Roger Davis',
-    'William George',
-    'Andrew Torres',
-    'Billy Campbell',
-    'Alan King',
-    'Benjamin Wilson',
-    'Bill Griffin',
-    'Dennis Smith',
-    'Billy Griffith'
-  ]
-  let expect = [
-    'Bill Griffin',
-    'Billy Griffith',
-    'Billy Campbell',
-    'Alan King',
-    'William George',
-    'Carl Martinez',
-    'Roger Davis',
-    'Dennis Smith',
-    'Benjamin Wilson',
-    'Andrew Torres'
-  ]
-  let levSorted = levenSort(nameAry, sourceName)
-  assert.deepEqual(levSorted, expect)
-})
+runTests('Testing leven-sort', () => {
+  test(`sorts simple text array`, () => {
+    const sourceName = 'Bill Griffin'
 
-test(`sorts array of objects, with secondary sort`, (assert) => {
-  const sourceFirst = 'Bill'
-  const sourceLast = 'Griffin'
-  const nameObjAry = [
-    { first: 'Carl', last: 'Martinez' },
-    { first: 'Roger', last: 'Davis' },
-    { first: 'William', last: 'George' },
-    { first: 'Andrew', last: 'Torres' },
-    { first: 'Billy', last: 'Campbell' },
-    { first: 'Alan', last: 'King' },
-    { first: 'Benjamin', last: 'Wilson' },
-    { first: 'Bill', last: 'Griffin' },
-    { first: 'Dennis', last: 'Smith' },
-    { first: 'Billy', last: 'Griffith' }
-  ]
-  let expect = [
-    { first: 'Bill', last: 'Griffin' },
-    { first: 'Billy', last: 'Griffith' },
-    { first: 'Billy', last: 'Campbell' },
-    { first: 'Carl', last: 'Martinez' },
-    { first: 'William', last: 'George' },
-    { first: 'Alan', last: 'King' },
-    { first: 'Roger', last: 'Davis' },
-    { first: 'Dennis', last: 'Smith' },
-    { first: 'Andrew', last: 'Torres' },
-    { first: 'Benjamin', last: 'Wilson' }
-  ]
-  let levSorted = levenSort(nameObjAry, sourceFirst, 'first', sourceLast, 'last')
-  assert.deepEqual(levSorted, expect)
-})
+    const nameAry = [
+      'Carl Martinez',
+      'Roger Davis',
+      'William George',
+      'Andrew Torres',
+      'Billy Campbell',
+      'Alan King',
+      'Benjamin Wilson',
+      'Bill Griffin',
+      'Dennis Smith',
+      'Billy Griffith'
+    ]
 
-test(`sorts array of objects by multiple keys`, (assert) => {
-  const source = 'Bill'
-  const nameObjAry = [
-    { first: 'Carl', last: 'Martinez' },
-    { first: 'Roger', last: 'Davis' },
-    { first: 'William', last: 'George' },
-    { first: 'Andrew', last: 'Torres' },
-    { first: 'Billy', last: 'Campbell' },
-    { first: 'Alan', last: 'King' },
-    { first: 'Benjamin', last: 'Wilson' },
-    { first: 'Bill', last: 'Griffin' },
-    { first: 'Dennis', last: 'Smith' },
-    { first: 'Billy', last: 'Griffith' }
-  ]
-  let expect = [
-    { first: 'Bill', last: 'Griffin' },
-    { first: 'Billy', last: 'Campbell' },
-    { first: 'Billy', last: 'Griffith' },
-    { first: 'Carl', last: 'Martinez' },
-    { first: 'Alan', last: 'King' },
-    { first: 'William', last: 'George' },
-    { first: 'Benjamin', last: 'Wilson' },
-    { first: 'Dennis', last: 'Smith' },
-    { first: 'Roger', last: 'Davis' },
-    { first: 'Andrew', last: 'Torres' }
-  ]
-  let levSorted = levenSort(nameObjAry, source, ['first', 'last'])
-  assert.deepEqual(levSorted, expect)
+    const expect = [
+      'Bill Griffin',
+      'Billy Griffith',
+      'Billy Campbell',
+      'Alan King',
+      'William George',
+      'Carl Martinez',
+      'Roger Davis',
+      'Dennis Smith',
+      'Benjamin Wilson',
+      'Andrew Torres'
+    ]
+
+    const levSorted = levenSort(nameAry, sourceName)
+
+    return test('samesies', JSON.stringify(levSorted), JSON.stringify(expect))
+  })
+
+  test(`sorts array of objects, with secondary sort`, () => {
+    const sourceFirst = 'Bill'
+    const sourceLast = 'Griffin'
+
+    const nameObjAry = [
+      { first: 'Carl', last: 'Martinez' },
+      { first: 'Roger', last: 'Davis' },
+      { first: 'William', last: 'George' },
+      { first: 'Andrew', last: 'Torres' },
+      { first: 'Billy', last: 'Campbell' },
+      { first: 'Alan', last: 'King' },
+      { first: 'Benjamin', last: 'Wilson' },
+      { first: 'Bill', last: 'Griffin' },
+      { first: 'Dennis', last: 'Smith' },
+      { first: 'Billy', last: 'Griffith' }
+    ]
+
+    const expect = [
+      { first: 'Bill', last: 'Griffin' },
+      { first: 'Billy', last: 'Griffith' },
+      { first: 'Billy', last: 'Campbell' },
+      { first: 'Carl', last: 'Martinez' },
+      { first: 'William', last: 'George' },
+      { first: 'Alan', last: 'King' },
+      { first: 'Roger', last: 'Davis' },
+      { first: 'Dennis', last: 'Smith' },
+      { first: 'Andrew', last: 'Torres' },
+      { first: 'Benjamin', last: 'Wilson' }
+    ]
+
+    const levSorted = levenSort(nameObjAry, sourceFirst, 'first', sourceLast, 'last')
+
+    return test('samesies', JSON.stringify(levSorted), JSON.stringify(expect))
+  })
+
+  test(`sorts array of objects by multiple keys`, () => {
+    const source = 'Bill'
+
+    const nameObjAry = [
+      { first: 'Carl', last: 'Martinez' },
+      { first: 'Roger', last: 'Davis' },
+      { first: 'William', last: 'George' },
+      { first: 'Andrew', last: 'Torres' },
+      { first: 'Billy', last: 'Campbell' },
+      { first: 'Alan', last: 'King' },
+      { first: 'Benjamin', last: 'Wilson' },
+      { first: 'Bill', last: 'Griffin' },
+      { first: 'Dennis', last: 'Smith' },
+      { first: 'Billy', last: 'Griffith' }
+    ]
+
+    const expect = [
+      { first: 'Bill', last: 'Griffin' },
+      { first: 'Billy', last: 'Campbell' },
+      { first: 'Billy', last: 'Griffith' },
+      { first: 'Carl', last: 'Martinez' },
+      { first: 'Alan', last: 'King' },
+      { first: 'William', last: 'George' },
+      { first: 'Benjamin', last: 'Wilson' },
+      { first: 'Dennis', last: 'Smith' },
+      { first: 'Roger', last: 'Davis' },
+      { first: 'Andrew', last: 'Torres' }
+    ]
+
+    const levSorted = levenSort(nameObjAry, source, ['first', 'last'])
+
+    return test('samesies', JSON.stringify(levSorted), JSON.stringify(expect))
+  })
 })
